cb57368259205d0c2ba1ea27f90c99c18c20ce8e
Santiago <alvarezsantiagonicolas105@hotmail.com>
2025-07-22 19:31:43 -0300

    Primer push

---
+++ script.js
@@ -0,0 +1,44 @@
+let otroEstudiante = 1
+let nombre;
+let nota1;
+let nota2;
+let notaFinal;
+const promediarNotas = (a, b) => (a + b) / 2;
+    
+while (otroEstudiante === 1) {
+    nombre = prompt("Introduce el nombre del alumno:");
+    do {
+        nota1 = parseFloat(prompt("Introduce la nota del primer parcial:"));
+        if (isNaN(nota1) || nota1 < 0 || nota1 > 10) {
+            alert("Por favor, introduce un número válido para la nota del primer parcial.");
+        }
+    } while (isNaN(nota1) || nota1 < 0 || nota1 > 10);
+
+    do {
+        nota2 = parseFloat(prompt("Introduce la nota del segundo parcial:"));
+        if (isNaN(nota2) || nota2 < 0 || nota2 > 10) {
+            alert("Por favor, introduce un número válido para la nota del segundo parcial.");
+        }
+    } while (isNaN(nota2) || nota2 < 0 || nota2 > 10);
+
+    notaFinal = promediarNotas(nota1, nota2);
+
+    if (notaFinal >= 7) {
+        console.log(`${nombre} promociona con una nota de ${notaFinal}.`);
+    }else if (notaFinal >= 4) {
+        console.log(`${nombre} va a final con una nota de ${notaFinal}.`);
+    }else {
+        console.log(`${nombre} repite con una nota de ${notaFinal}.`);
+    }
+
+    do {
+        otroEstudiante = parseInt(prompt("¿Deseas ingresar otro estudiante? (1 para sí, 0 para no):"));
+        if (otroEstudiante !== 0 && otroEstudiante !== 1) {
+            alert("Por favor, introduce un número válido para la instruccion solicitada.");
+        }
+    } while (otroEstudiante !== 0 && otroEstudiante !== 1);
+   
+}
+
+console.log(`Gracias por usar el sistema de evaluación.`);
+
