d68aba0d07a8d31748d5c6108116d880fef8a072
Santiago <alvarezsantiagonicolas105@hotmail.com>
2025-07-23 10:25:15 -0300

    Segundo push

--- script.js
+++ script.js
@@ -3,10 +3,12 @@ let nombre;
 let nota1;
 let nota2;
 let notaFinal;
+const bienvenida = () => {
+    console.log("Bienvenido al sistema de evaluación de estudiantes.");
+    console.log("Por favor, sigue las instrucciones para ingresar las notas de los estudiantes.");
+}
 const promediarNotas = (a, b) => (a + b) / 2;
-    
-while (otroEstudiante === 1) {
-    nombre = prompt("Introduce el nombre del alumno:");
+const recopilarNotas = () => {
     do {
         nota1 = parseFloat(prompt("Introduce la nota del primer parcial:"));
         if (isNaN(nota1) || nota1 < 0 || nota1 > 10) {
@@ -20,9 +22,9 @@ while (otroEstudiante === 1) {
             alert("Por favor, introduce un número válido para la nota del segundo parcial.");
         }
     } while (isNaN(nota2) || nota2 < 0 || nota2 > 10);
-
-    notaFinal = promediarNotas(nota1, nota2);
-
+    return [nota1, nota2];
+}
+const analizarSituacion = (notaFinal) => {
     if (notaFinal >= 7) {
         console.log(`${nombre} promociona con una nota de ${notaFinal}.`);
     }else if (notaFinal >= 4) {
@@ -30,15 +32,28 @@ while (otroEstudiante === 1) {
     }else {
         console.log(`${nombre} repite con una nota de ${notaFinal}.`);
     }
-
+}
+const Estudiantes = () => { 
     do {
         otroEstudiante = parseInt(prompt("¿Deseas ingresar otro estudiante? (1 para sí, 0 para no):"));
         if (otroEstudiante !== 0 && otroEstudiante !== 1) {
             alert("Por favor, introduce un número válido para la instruccion solicitada.");
         }
     } while (otroEstudiante !== 0 && otroEstudiante !== 1);
-   
+    return otroEstudiante;
+}
+const programa = () => {
+    
+    bienvenida();
+    while (otroEstudiante === 1) {
+        nombre = prompt("Introduce el nombre del alumno:");
+        recopilarNotas();
+        notaFinal = promediarNotas(nota1, nota2);
+        analizarSituacion(notaFinal);
+        otroEstudiante = Estudiantes();
+    }
+    console.log(`Gracias por usar el sistema de evaluación.`);
 }
 
-console.log(`Gracias por usar el sistema de evaluación.`);
 
+programa();
